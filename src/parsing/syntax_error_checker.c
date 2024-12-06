369081121713eaf91ffed9757ea029e7fb8c38b1
larasoft2 <raphaelle.couttenier@gmail.com>
2024-12-06 17:20:07 +0100

    comments special characters, a interpreter entre quotes..., tokenizer file created mais pas avance 06/12

--- src/parsing/syntax_error_checker.c
+++ src/parsing/syntax_error_checker.c
@@ -6,7 +6,7 @@
 /*   By: racoutte <racoutte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/04 16:24:43 by racoutte          #+#    #+#             */
-/*   Updated: 2024/12/06 13:08:53 by racoutte         ###   ########.fr       */
+/*   Updated: 2024/12/06 15:45:24 by racoutte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,11 @@
 
 int	syntax_error_checker(char *input)
 {
-	if (not_handled_char_input(input) == NOT_HANDLED_CHARACTER)
-		return (EXIT_FAILURE);
 	if (check_empty_input(input) == EMPTY_STRING)
 		return (EXIT_FAILURE);
 	if (check_only_whitespace_intput(input) == INPUT_ONLY_WHITESPACE)
 		return (EXIT_FAILURE);
 	if (check_first_character_is_special_without_error(input) == FIRST_CHARACTER_IS_SPECIAL)
-		return (EXIT_FAILURE);
-	if (number_redir_right(input) == EXIT_FAILURE)
-		return (EXIT_FAILURE);
-	if (number_redir_left(input) == EXIT_FAILURE)
 		return (EXIT_FAILURE);
 	if (input_starts_or_ends_with_pipe(input) == STARTS_OR_ENDS_WITH_PIPE_REDIR)
 		return (EXIT_FAILURE);
