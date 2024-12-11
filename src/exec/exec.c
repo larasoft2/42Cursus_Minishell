698616bed37bad53734a79b5be1f916376e37d4e
lutothena <jd.svg@hotmail.com>
2024-12-11 03:42:02 +0100

    Update exec.c

--- src/exec/exec.c
+++ src/exec/exec.c
@@ -13,7 +13,7 @@
 #include "minishell_exec.h"
 
 // Les arguments seront places dans un tableau **,
-// donc je verifie si le premier argument fait ref a un builtin
+// donc je verifie si un arg du tab fait ref a un builtin
 
 // // The  getenv()  function searches the environment list to find the
 // environment variable name,
@@ -21,20 +21,27 @@
 
 // int	ft_isbuiltin(t_ast *ast)
 // {
-// 	if (ft_strcmp(ast->arg[0], "echo") == 0)
-// 		ft_echo(tmp);
-// 	// else if (ft_strcmp(ast->arg[0], "cd") == 0)
-// 	//	ft_cd(tmp);
-// 	else if (ft_strcmp(ast->arg[0], "pwd") == 0)
+//  int i;
+//
+//  i = 0;
+//  while (arg[i])
+//  {
+//    if (ft_strcmp(ast->arg[i], "echo") == 0)
+// 	  ft_echo(tmp);
+// 	else if (ft_strcmp(ast->arg[i], "cd") == 0)
+// 	  ft_cd(tmp);
+// 	else if (ft_strcmp(ast->arg[i], "pwd") == 0)
 // 		ft_pwd(tmp);
-// 	// else if (ft_strcmp(ast->arg[0], "export") == 0)
-// 	//	ft_export(tmp);
-// 	// else if (ft_strcmp(ast->arg[0], "unset") == 0)
-// 	//	ft_unset(tmp);
-// 	// else if (ft_strcmp(ast->arg[0], "env") == 0)
-// 	//	ft_env(tmp);
-// 	else if (ft_strcmp(ast->arg[0], "cd") == 0)
+// 	else if (ft_strcmp(ast->arg[i], "export") == 0)
+// 	  ft_export(tmp);
+// 	else if (ft_strcmp(ast->arg[i], "unset") == 0)
+// 	  ft_unset(tmp);
+// 	else if (ft_strcmp(ast->arg[i], "env") == 0)
+// 	  ft_env(tmp);
+// 	else if (ft_strcmp(ast->arg[i], "cd") == 0)
 // 		ft_exit(tmp);
 // 	else
-// 		return (-1);
+//    i++;
+//  }
+//  return (-1);
 // }
