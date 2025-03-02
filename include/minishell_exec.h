2a03c2fc7764d0e311c84a675712ced33efc4a87
lutothena <lusavign@NEBULA.>
2025-03-03 00:08:43 +0100

    premerge commit

--- include/minishell_exec.h
+++ include/minishell_exec.h
@@ -104,34 +104,35 @@ void				ft_init(t_exec *ex, int *std_dup);
 void    			restore_fds(int *std_dup);
 void				ft_close_fds(int fd);
 void				ft_close_fd(int *pipefd);
-int ft_open(t_exec *ex, int *fd_in);
-void				ft_open_heredocs(t_exec *ex, int pipefd);
+void				skip_redirections(t_exec **current);
+
 
 t_exec				*get_next_exec_token(t_exec *ex);
 t_exec				*find_next_valid_token(t_exec *ex);
 
 // EXEC//
 int					exec_builtin(t_exec *ex, t_env **env, int *std_dup);
-int					is_builtin(t_exec *ex);
+int					ft_open(t_exec *ex, int *fd_in);
 int					handle_heredoc(t_exec *ex);
+int					handle_redir(t_exec *ex);
+int					handle_redir_in_pipe(t_exec *ex, int pipefd);
+int					is_builtin(t_exec *ex);
+int					redir_in(t_exec *ex, int *fd_in);
+int					redir_out(t_exec *ex);
 int					setup_pipe(int pipefd[2]);
 
-char				**put_env_in_ar(t_env *envp);
-char				*is_path_exec(char *cmd, char **full_paths);
 char				*get_path(t_env *env, char *cmd);
+char				*is_path_exec(char *cmd, char **full_paths);
+char				**put_env_in_ar(t_env *envp);
 
-void				skip_redirections(t_exec **current);
-void				setup_io_for_command(t_pipes *p);
-void				handle_empty_pipe(t_pipes *p);
-int				redir_out(t_exec *ex);
-int				redir_in(t_exec *ex, int *fd_in);
-int				handle_redir(t_exec *ex);
-int				handle_redir_in_pipe(t_exec *ex, int pipefd);
-void				handle_pipes_no_redir(t_exec *ex, t_env **env, int *std_dup);
-void				handle_pipes_if_redir(t_exec *ex, t_env **env, int *std_dup);
 void				ft_exec(t_exec *ex, t_env **env);
 void				ft_fork(t_exec *cmd, t_env **env, int *std_dup);
+void				ft_open_heredocs(t_exec *ex, int pipefd);
 void    			ft_process(t_env **env, t_exec *ex);
+void				handle_empty_pipe(t_pipes *p);
+void				handle_pipes_if_redir(t_exec *ex, t_env **env, int *std_dup);
+void				handle_pipes_no_redir(t_exec *ex, t_env **env, int *std_dup);
+void				setup_io_for_command(t_pipes *p);
 
 // FREE//
 void				*ft_free_array(char **ar);
